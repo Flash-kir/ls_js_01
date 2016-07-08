d637078b8f19f357cf3225818e26c9754d03c217
Alexey Ogloblin <flash-kir@yandex.ru>
2016-07-08 15:37:20 +0300

    hw1_v2

--- hw1.js
+++ hw1.js
@@ -1,10 +1,8 @@
-consoleRec(['я', 'умею', 'писать', 'рекурсивные', 'функции'], 123);
+consoleRec(['я', 'умею', 'писать', 'рекурсивные', 'функции'], 0);
 
-function consoleRec(){
-	if (arguments[0] && arguments[0][0]){
-		console.log(arguments[0][0]);
-    arguments[0].splice(0,1);
-    consoleRec(arguments[0]);
+function consoleRec(array, num){
+	if (array.length > num){
+		console.log(array[num]);
+    consoleRec(array, num+1);
   }
 }
-
