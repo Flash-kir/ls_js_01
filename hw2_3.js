7906e2aa4fff388a2ff8de90827efdbd8fe9a228
Alexey Ogloblin <flash-kir@yandex.ru>
2016-07-11 10:14:22 +0300

    third part of 2nd home work

---
+++ hw2_3.js
@@ -0,0 +1,50 @@
+function calculator(n){
+  function sum(){
+    res = n;
+    for (let i = 0; i < arguments.length; i++) {
+      res += arguments[i];
+    };
+    return res;
+  };
+
+  function dif(){
+    res = n;
+    for (let i = 0; i < arguments.length; i++) {
+      res -= arguments[i];
+    };
+    return res;
+  };
+
+   function div(){
+     try {
+       res = n;
+       for (let i = 0; i < arguments.length; i++) {
+         if (arguments[i] != 0){
+           res /= arguments[i];
+         }
+         else {
+           throw new Error("На ноль делить нельзя!!");
+         }
+       };
+       return res;
+     }
+     catch(e) {console.log(e.message);}
+  };
+
+  function mul(){
+    res = n;
+    for (let i = 0; i < arguments.length; i++) {
+      res *= arguments[i];
+    };
+    return res;
+  };
+
+  return {
+    sum: sum, 
+    dif: dif,
+    div: div,
+    mul: mul
+  }  
+};
+
+module.exports = calculator;
