1bafbc74873cc2efd1cddd51ac4e67450eda0477
Alexey Ogloblin <flash-kir@yandex.ru>
2016-07-11 09:41:54 +0300

    first part of 2nd home work

---
+++ hw2_1.js
@@ -0,0 +1,17 @@
+function isAllTrue(source, filterFn){
+  try{
+    if (source.length > 0) {
+      let res = true;
+      for (var i = 0; i < source.length; i++){
+        res = res && filterFn(source[i]);
+      };
+  return res;
+    }
+    else {
+      throw new Error("Массив не должен быть пустым!");
+    }
+  }
+  catch(e){console.log(e.message);}
+};
+
+module.exports = isAllTrue;
