dcdcc79b4de8936535378dbec2ba6ad1cdd6f00b
Alexey Ogloblin <flash-kir@yandex.ru>
2016-07-26 11:09:19 +0300

    hw 6 part 3 version 1

--- hw6/bundle.js
+++ hw6/bundle.js
@@ -48,24 +48,40 @@
 
 	let fn = __webpack_require__(1),
 	    btn = document.getElementById("getTwn"),
+	    search = document.getElementById("tSearch"),
+	    sBlk = document.getElementById("sBlk"),
 	    url_town = "https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json";
 
 	window.my_timer = fn.timer;
 
-	btn.addEventListener('click', function getTowns() {
+	btn.addEventListener('click', function () {
 	  fn.ajaxGet(url_town).then(function (resp) {
 	    let towns = fn.sortList(resp);
-	    if (document.getElementById("town_list")) {
-	      document.getElementById("town_list").remove();
-	    }
-	    let ul_el = document.createElement("UL");
-	    if (towns.length) {
-	      ul_el.id = "town_list";
-	      document.body.appendChild(ul_el);
-	    }
+	    let ul_el = document.getElementById("town_list");
+	    ul_el.innerHTML = "";
 	    for (let i = 0; i < towns.length; i++) {
 	      let li_el = document.createElement("LI");
 	      li_el.textContent = towns[i];
+	      ul_el.appendChild(li_el);
+	    }
+	  });
+	});
+
+	search.addEventListener('keydown', function () {
+	  fn.ajaxGet(url_town).then(function (resp) {
+	    let towns = fn.sortList(resp);
+	    let ul_el = document.getElementById("sList");
+	    ul_el.innerHTML = "";
+	    for (let i = 0; i < towns.length; i++) {
+	      if (towns[i].toLowerCase().indexOf(search.value.toLowerCase()) > 0) {
+	        let li_el = document.createElement("LI");
+	        li_el.textContent = towns[i];
+	        ul_el.appendChild(li_el);
+	      }
+	    }
+	    if (!ul_el.childElementCount) {
+	      let li_el = document.createElement("LI");
+	      li_el.textContent = "Совпадений не найдено";
 	      ul_el.appendChild(li_el);
 	    }
 	  });
