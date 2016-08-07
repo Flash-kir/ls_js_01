9456a666dceeccdc982d6b7ec2170758851e883c
Alexey Ogloblin <flash-kir@yandex.ru>
2016-08-07 08:22:26 +0300

    hw8_1 bugfix

--- hw8/hw8_1/js/main.js
+++ hw8/hw8_1/js/main.js
@@ -4,29 +4,36 @@ let fn = require("./hw6.js"),
   search = document.getElementById("tSearch"),
   sBlk = document.getElementById("sBlk"),
   sList = document.getElementById("sList"),
-  url_town = "https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json";
+  url_town = "https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json",
+  towns;
 
 window.my_timer = fn.timer;
 
-search.addEventListener('keydown', 
-    function () {
-      fn.ajaxGet(url_town).then(
-      function (resp) {
-        let towns = fn.sortList(resp), list = [];
-        
-        for (let i = 0; i < towns.length; i++) {
-          if (towns[i].toLowerCase().indexOf(search.value.toLowerCase()) >= 0){
-            list.push({name:towns[i]});
-          }
-        }
-        if (list.length == 0) {
-          list.push({name:"Совпадений не найдено"});
-        }
-        
-        let source = document.getElementById('townList').innerHTML;
-        let templateFn = Handlebars.compile(source);
-
-        sBlk.innerHTML = templateFn({towns: list});
-      })
+function tfilter() {
+  let list = [];
+  
+  for (let i = 0; i < towns.length; i++) {
+    if (towns[i].toLowerCase().indexOf(search.value.toLowerCase()) >= 0){
+      list.push({name:towns[i]});
+    }
   }
-);
+  if (list.length == 0) {
+    list.push({name:"Совпадений не найдено"});
+  }
+  
+  let source = document.getElementById('townList').innerHTML;
+  let templateFn = Handlebars.compile(source);
+
+  sBlk.innerHTML = templateFn({towns: list});
+}
+
+document.addEventListener("DOMContentLoaded", ()=>{
+  fn.ajaxGet(url_town).then(
+    function (resp) {
+      towns = fn.sortList(resp);
+      tfilter();
+    }
+  )
+});
+
+search.addEventListener('keyup', tfilter);
