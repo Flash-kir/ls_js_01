7f2ba3dda0c0b80e2dbe4285ff040a01b44538bb
Alexey Ogloblin <flash-kir@yandex.ru>
2016-09-02 21:34:37 +0300

    status bar

--- hw11/controller.js
+++ hw11/controller.js
@@ -20,19 +20,34 @@ var Controller = {
         });
     },
     photosRoute: function() {
+        let bar = document.body.querySelector(".progress"),
+            bar_albums = bar.querySelector(".progress-albums"),
+            bar_photos = bar.querySelector(".progress-photos"),
+            bar_comments = bar.querySelector(".progress-comments");
+        bar_albums.style = "";
+        bar_photos.style = "";
+        bar_comments.style = "";
+        bar.style.display = "block";
         Model.getPhotoAlbums().then(function(albums){
-            let albums_dict = {}, count = 0;
+            let albums_dict = {}, count = 0, progress = 0, step = 200/albums.items.length, color = "";
             for (let i = 0; i < albums.items.length; i++) {
                 let album = albums.items[i];
-                albums_dict[album.id] = {id: album.id,title: album.title, items: [], count: album.size};
-                count += album.size;
+                if (album.id != -15) {
+                    albums_dict[album.id] = {id: album.id,title: album.title, items: [], count: album.size};
+                    count += album.size;
+                }
+                progress += step;
+                if (progress > 199) {
+                    color = "background-color: green; color: white";
+                }
+                bar_albums.style = "width:" + progress + "px;" + color;
             }
             return [albums_dict, count];
         }).then(resp => {
             document.getElementById("results").innerHTML = View.render('albums', resp[0] );
             return resp;
         }).then(resp => {
-            let promise_list = [];
+            let promise_list = [], progress = 0, step = 200/resp[1], color = "";
             for (let i = 0; i < resp[1]/200 + 1; i++) {
                 promise_list.push(
                     Model.getPhotos(i * 200).then(function(photos) {
@@ -47,12 +62,17 @@ var Controller = {
                         el.classList = ['row'];
                         el.innerHTML = View.render('photos', photo);
                         document.getElementById(photo.album_id).appendChild(el);
+                        progress += step;
+                        if (progress > 199) {
+                            color = "background-color: green; color: white";
+                        }
+                        bar_photos.style = "width:" + progress + "px;" + color;
                     }
                 }
                 return true;
             }).then(
                 Model.callApi('photos.getAllComments', {extended: 1, offset: 0, count: 1, v:"5.53"}).then((resp) => {
-                    let promise_list = [], users = [];
+                    let promise_list = [], users = [], progress = 0, step = 200/resp.count, color = "";
                     for (let i = 0; i < resp.count/100; i++) {
                         promise_list.push(
                             Model.getComments(i * 100).then(function(comments) {
@@ -77,6 +97,11 @@ var Controller = {
                                     photo_el.innerHTML = '';
                                 }
                                 photo_el.appendChild(comment_el);
+                                progress += step;
+                                if (progress > 199) {
+                                    color = "background-color: green; color: white";
+                                }
+                                bar_comments.style = "width:" + progress + "px;" + color;
                             }
                         }
                     }).then(response => {
@@ -89,6 +114,7 @@ var Controller = {
                             );
                         }
                         Promise.all(promise_list).then(users => {
+                            setTimeout(bar.style.display = "", 0);
                             for (let i = 0; i < users.length; i++) {
                                 for (let j = 0; j < users[i].length; j++) {
                                     let user = users[i][j],
