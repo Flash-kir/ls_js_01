911e78c4b6e62948fb525895ec5945f7edc7dca7
Alexey Ogloblin <flash-kir@yandex.ru>
2016-09-06 13:59:15 +0300

    chat final v1

--- hw14/js/index.js
+++ hw14/js/index.js
@@ -1,38 +1,138 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 // создать подключение
-var socket = new WebSocket("ws://localhost:8081"),
-    View = {
-    render: function(templateName, model) {
-        templateName = templateName + 'Template';
+var View = {
+        render: function(templateName, model) {
+            templateName = templateName + 'Template';
+    
+            var templateElement = document.getElementById(templateName),
+                templateSource = templateElement.innerHTML,
+                renderFn = Handlebars.compile(templateSource);
+            return renderFn(model);
+        }
+    },
+    user_name = document.querySelector(".user-name"),
+    user_nick = document.querySelector(".user-nick"),
+    load_area = document.querySelector(".load-area"),
+    user_list = document.querySelector(".user-list"),
+    socket;
 
-        var templateElement = document.getElementById(templateName),
-            templateSource = templateElement.innerHTML,
-            renderFn = Handlebars.compile(templateSource);
-        // console.log(renderFn(model));
-        return renderFn(model);
-    }
-};
+  function handleFileSelect(evt) {
+    evt.stopPropagation();
+    evt.preventDefault();
+
+    var files = evt.dataTransfer.files,
+        reader = new FileReader();
+      console.log(files[0]);
+      if (files[0].size > 512 * 1024) {
+          load_area.innerHTML = "Файл не должен превышать 512 Kb";
+        return false;
+      } else if (files[0].type != "image/jpeg") {
+          load_area.innerHTML = "Загружайте фото в формате JPEG";
+        return false;
+      }
+        reader.onload = (function() {
+            return function(e) {
+                var img = document.createElement("img");
+                img.src = e.target.result;
+                load_area.innerHTML = "";
+                load_area.appendChild(img);
+            };
+        })(files[0]);
+
+      reader.readAsDataURL(files[0]);
+  }
+  function handleDragOver(evt) {
+    evt.stopPropagation();
+    evt.preventDefault();
+    evt.dataTransfer.dropEffect = 'copy';
+  }
+  function load_img(send) {
+      if ( load_area.querySelector("img") ) {
+          if (send) {
+              socket.send(JSON.stringify({type: "photo",
+                  user_name: user_name.value,
+                  user_nick: user_nick.value,
+                  src: load_area.querySelector("img").src,
+                  user_photo: "media/img/logos/" + user_nick.value + ".jpg"
+              }));
+          }
+          load_area.innerHTML = "<p>Перетащите сюда фото</p>";
+      }
+      top(id("load"), -500);
+  }
+  function showMessage(message) {
+      var messageElem = document.createElement('div');
+      messageElem.classList.add("message-blk");
+      messageElem.innerHTML = View.render('message', message);
+      id('subscribe').appendChild(messageElem);
+  }
+  function id(name) {
+      return document.getElementById(name)
+  }
+  function top(el, top) {
+      el.style.top = top + "px";
+  }
+  function changePhotos(msg) {
+      var photos = document.querySelectorAll('[data-photo="' + msg.user_nick + '"]');
+      for (var i = 0; i < photos.length; i++) {
+          var photo = photos[i];
+          photo.src = msg.user_photo;
+      }
+  }
+  function writeList(msg) {
+      id("user-list").querySelector(".number").innerHTML = msg.user.length;
+      id("user-list").querySelector(".list").innerHTML = msg.user.join("");
+  }
+
+  load_area.addEventListener('dragover', handleDragOver, false);
+  load_area.addEventListener('drop', handleFileSelect, false);
+  id("load-btn").addEventListener('click', () => load_img(true));
+  id("cancel-btn").addEventListener('click', () => load_img(false));
+
 // отправить сообщение из формы publish 
 document.addEventListener('click', function(event) {
-  if ( event.target == document.getElementById("submit") ) {
-    var outgoingMessage = document.getElementById("text").value;
-    document.getElementById("text").value = "";
-    socket.send(outgoingMessage);
-    return false;
+  if ( event.target == id("submit") ) {
+      var outgoingMessage = JSON.stringify({type: "text", text: document.getElementById("text").value,
+          user_name: user_name.value,
+          user_nick: user_nick.value,
+          user_photo: id("user-photo").src,
+          date: ""});
+      id("text").value = "";
+      socket.send(outgoingMessage);
+  } else if ( event.target == id("login-btn") ) {
+      if (user_name.value != '' && user_nick.value != '') {
+          top(id("login"), -500);
+          user_list.style.display = "block";
+          id("user-name").innerText = user_name.value;
+          id("user-photo").dataset.photo = user_nick.value;
+          
+          //Создаем подключение
+          socket = new WebSocket("ws://localhost:8081");
+          socket.onopen = () => {
+              socket.send(JSON.stringify({type: 'login', user_nick: user_nick.value, user_name: user_name.value}))
+          };
+
+          // обработчик входящих сообщений
+          socket.onmessage = function(event) {
+              var incomingMessage = JSON.parse(event.data);
+              switch (incomingMessage.type) {
+                  case "text":
+                      showMessage(incomingMessage);
+                      break;
+                  case "photo":
+                      changePhotos(incomingMessage);
+                      break;
+                  case "login":
+                      writeList(incomingMessage);
+                      break;
+              }
+          };
+
+          id("user-photo").addEventListener('click', () => top(id("load"), 0))
+      }
   }
+    return false;
 });
 
-// обработчик входящих сообщений
-socket.onmessage = function(event) {
-  var incomingMessage = event.data;
-  showMessage(incomingMessage);
-};
 
-// показать сообщение в div#subscribe
-function showMessage(message) {
-  var messageElem = document.createElement('div');
-  messageElem.classList.add("message-blk");
-  messageElem.innerHTML = View.render('message', {name: "Имя Чела", nick_name: "nick", photo: 'http://lorempixel.com/50/50/people', text: message, time: new Date().toLocaleString()});
-  document.getElementById('subscribe').appendChild(messageElem);
-}
 },{}]},{},[1])
--- hw14/server.js
+++ hw14/server.js
@@ -1,7 +1,8 @@
-var WebSocketServer = new require('ws');
+var WebSocketServer = new require('ws'),
+    fs = require('fs');
 
 // подключенные клиенты
-var clients = {};
+var clients = {}, list = [], messages = [], users = {};
 
 // WebSocket-сервер на порту 8081
 var webSocketServer = new WebSocketServer.Server({
@@ -10,20 +11,54 @@ var webSocketServer = new WebSocketServer.Server({
 webSocketServer.on('connection', function(ws) {
 
   var id = Math.random();
-  clients[id] = ws;
-  console.log("новое соединение " + id);
+  clients[id] = {};
+  clients[id]['ws'] = ws;
+  for (var i = 0; i < messages.length; i++) {
+    ws.send(messages[i]);
+  }
 
   ws.on('message', function(message) {
-    console.log('получено сообщение ' + message);
+    var msg = JSON.parse(message);
+    msg.date = new Date().toLocaleString();
+    if (!users[msg.user_nick]) {
+      users[msg.user_nick] = {}
+    }
+    if (msg.type == "login") {
+      if (list.indexOf('<p>' + msg.user_name + '</p>') == -1) {
+        list.push('<p>' + msg.user_name + '</p>');
+        msg.user = list;
+        clients[id]['user'] = msg.user_name;
+      } else {
+        msg.logout = true
+      }
+    }
+    if (msg.type == "photo") {
+      fs.open("media/img/logos/" + msg.user_nick + ".jpg", "w", (err, fd) => {
+        fs.writeFile("media/img/logos/" + msg.user_nick + ".jpg", msg.src.replace(/^data:image\/(png|jpg|jpeg|gif);base64,/,""), 'base64');
+        users[msg.user_nick]['photo'] = "media/img/logos/" + msg.user_nick + ".jpg?" + Math.random();
+      });
+    }
+    if (users[msg.user_nick]) {
+      if (users[msg.user_nick]['photo'] != msg.user_photo && users[msg.user_nick]['photo']) {
+        msg.user_photo = users[msg.user_nick]['photo'];
+      }
+    }
 
+    message = JSON.stringify(msg);
+    console.log('получено сообщение ' + message);
+    messages.push(message);
     for (var key in clients) {
-      clients[key].send(message);
+      clients[key]['ws'].send(message);
     }
   });
 
   ws.on('close', function() {
     console.log('соединение закрыто ' + id);
+    list.splice(list.indexOf(clients[id].user), 1);
     delete clients[id];
+    for (var key in clients) {
+      clients[key]['ws'].send(JSON.stringify({type: "login", user: list}));
+    }
   });
 
 });
